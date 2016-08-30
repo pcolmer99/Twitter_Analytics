c6dd60c1d816aa03a1b198dde796b12984173d78
Paul Colmer <pcolmer99@gmail.com>
2016-08-30 11:12:06 +1000

    Tweaks to the Import Code

--- Twitter_Analytics_Import.R
+++ Twitter_Analytics_Import.R
@@ -20,10 +20,21 @@ AccessToken <- readLines (con,1)        ## Read first line
 AccessSecret <- readLines (con,1)     ## Read next line
 close(con)
 
-## Authenticate with Twitter using keys and tokens
+## Setup the OAuth (Open Authentication and Authorisation) with Twitter 
+## using keys and tokens from the Twitter application.
 setup_twitter_oauth(TwitKey, TwitSecret, AccessToken, AccessSecret)
 
 ## Setup Search String, number of tweets and load into tweets variable
 search.string <- "#MyWorkStyle"
-no.of.tweets <- 50
+no.of.tweets <- 100
 tweets <- searchTwitter(search.string, n=no.of.tweets, lang="en")
+
+## Create a dataframe based on the results
+df <- do.call("rbind", lapply(tweets, as.data.frame))
+
+## Count up the posts for each twitter user (screenName)
+twitcount <- table(df$screenName)
+
+## Show the counts in a Barplot - las=2 turn x axis lables vertical
+##   cex.names=0.5 changes the font size of the x axis text
+barplot(twitcount, las=2, cex.names=0.5)
